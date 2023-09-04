b5e5978b4a491f78bc1373025dfc1122ad2c1806
33kumarram <33kumarram@gmail.com>
2023-09-04 23:38:42 +0530

    made homepage responsive

--- src/Components/home/HomePage.jsx
+++ src/Components/home/HomePage.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Hidden } from "@mui/material";
 import { ChatListDisplay } from "../ChatList/ChatListDisplay";
 import { ChatSpace } from "../ChatSpace/ChatSpace";
-
+import { useSelector } from "react-redux";
 
 export const HomePage = () => {
+  const selectedChat = useSelector(state => state.selectedChat)
   return (
     <div>
       <Grid container disableEqualOverflow>
         <Grid item lg={3} sm={4} xs={12}>
-          <ChatListDisplay />
+          {selectedChat && selectedChat._id ?
+            <Hidden smDown>
+              <ChatListDisplay />
+            </Hidden>
+            :
+            <ChatListDisplay />
+          }
         </Grid>
         <Grid item lg={9} sm={8} xs={12}>
           <ChatSpace />
         </Grid>
       </Grid>
+      {
+        console.log(selectedChat == {}, selectedChat)
+      }
     </div>
   );
 };
